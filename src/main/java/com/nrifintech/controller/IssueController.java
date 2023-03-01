efd2557db77f96f71594b0979c4921a9bf666c55
Ms Yashika <yashika@trainee.nrifintech.com>
2023-03-01 14:01:45 +0530

    IssueService created

---
+++ src/main/java/com/nrifintech/controller/IssueController.java
@@ -0,0 +1,43 @@
+package com.nrifintech.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nrifintech.exception.ResourceNotFoundException;
+import com.nrifintech.model.Issue;
+import com.nrifintech.service.IssueService;
+
+@RestController
+@RequestMapping("/issue")
+public class IssueController {
+	@Autowired
+	IssueService is;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/showIssueList")
+	public List<Issue> getAllIssues() {
+		return is.getAllIssues();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addIssue")
+	public ResponseEntity<Issue> addIssue(@RequestBody Issue issue) throws ResourceNotFoundException {
+		return is.addIssue(issue);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteIssue/{issueId}")
+	public ResponseEntity<Issue> deleteIssue(@PathVariable int issueId) throws ResourceNotFoundException {
+		return is.deleteIssue(issueId);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/showIssue/{issueId}")
+	public ResponseEntity<Issue> getIssueById(@PathVariable int issueId) throws ResourceNotFoundException {
+		return is.getIssueByIssueId(issueId);
+	}
+
+}
--- src/main/java/com/nrifintech/service/IssueService.java
+++ src/main/java/com/nrifintech/service/IssueService.java
@@ -1,5 +1,33 @@
 package com.nrifintech.service;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.nrifintech.model.Issue;
+
+@Service
 public class IssueService {
+
+	public List<Issue> getAllIssues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResponseEntity<Issue> addIssue(Issue issue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResponseEntity<Issue> deleteIssue(int issueId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResponseEntity<Issue> getIssueByIssueId(int issueId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
