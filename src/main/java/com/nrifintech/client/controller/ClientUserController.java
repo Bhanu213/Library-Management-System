fe1f7371d9139a2aa506f9e2902092bb593da28e
Mohit Jaiswal <mohitj@trainee.nrifintech.com>
2023-03-06 10:15:32 +0530

    Monday commit

--- src/main/java/com/nrifintech/client/controller/ClientUserController.java
+++ src/main/java/com/nrifintech/client/controller/ClientUserController.java
@@ -42,7 +42,7 @@ public class ClientUserController {
 	@GetMapping("/dashboard")
 	public String dashboard(Model model) {
 		List<Book> books=bookService.getAllBooks();
-		System.out.println(books.get(0).getAuthor().getAuthorName());
+//		System.out.println(books.get(0).getAuthor().getAuthorName());
 		model.addAttribute("books", books);
 		System.out.println(books);
 		return "dashboard";
